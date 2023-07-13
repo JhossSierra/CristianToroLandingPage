0304f653bebe6020d2d071507bd782662729d372
Jhoss Sierra <joseangel184@gmail.com>
2023-07-13 16:46:46 -0400

    modal para la galería principal.

---
+++ main.js
@@ -0,0 +1,40 @@
+const body = document.getElementById('body')
+
+const modal=document.querySelector('.modal')
+
+const images = document.querySelectorAll('.vCenter img')
+
+console.log(images)
+
+const arrImages = Array.from(images)
+
+
+
+arrImages.map((image)=>image.addEventListener ('click',onClick))
+
+modal.addEventListener('click',closeModal)
+
+function onClick(e) {
+    console.log(e.target)
+    const src =e.target.src;
+    openmodal(src)
+}
+
+    function openmodal(src){
+        const modalImage = document.createElement('img')
+        body.className = 'overflow-hidden';
+        modal.style.display='flex'
+        modalImage.src=src
+        modalImage.className = 'modal-image'
+        modal.append(modalImage)
+        
+    }
+
+function closeModal() {
+   const modalImage=document.querySelector('.modal-image')
+   console.log(modalImage)
+    modal.style.display='none'
+    body.className= 'none'
+    modal.removeChild(modalImage)
+    
+}
