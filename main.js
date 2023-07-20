2b8d423ffd591e514f7a23d30c72b0710ceae7ed
Jhoss Sierra <joseangel184@gmail.com>
2023-07-20 15:58:57 -0400

    arreglando el modal

--- main.js
+++ main.js
@@ -17,7 +17,9 @@ function onClick(e) {
     const src =e.target.src;
     const srcArray=src.split('/')
     console.log(srcArray)
-    const newsrcArray= [srcArray[0],srcArray[1],srcArray[2],srcArray[3],srcArray[4],'HQ',srcArray[5]]
+    /* const newsrcArray= [srcArray[0],srcArray[1],srcArray[2],srcArray[3],srcArray[4],'HQ',srcArray[5]] */
+    const newsrcArray= [srcArray[0],srcArray[1],srcArray[2],srcArray[3],'HQ',srcArray[4]]
+    
     const newsrc= newsrcArray.join('/')
     console.log(newsrc)
     openmodal(newsrc)
@@ -25,9 +27,9 @@ function onClick(e) {
 
     function openmodal(src){
         const modalImage = document.createElement('img')
+        modalImage.src=src
         body.className = 'overflow-hidden';
         modal.style.display='flex'
-        modalImage.src=src
         modalImage.className = 'modal-image'
         modal.append(modalImage)
         
