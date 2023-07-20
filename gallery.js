82c00b5f84e756ecd743df406156b52d6e533c0d
Jhoss Sierra <joseangel184@gmail.com>
2023-07-20 19:52:26 -0400

    mejoras en la galería, cambio en la calidad de imagenes

--- gallery.js
+++ gallery.js
@@ -1,39 +1,34 @@
-const body = document.getElementById('body')
+const body = document.getElementById("body");
 
-const modal=document.querySelector('.modal')
+const modal = document.querySelector(".modal");
 
-const images = document.querySelectorAll('.galleryImg')
+const images = document.querySelectorAll(".galleryImg");
 
+const arrImages = Array.from(images);
 
-const arrImages = Array.from(images)
+arrImages.map((image) => image.addEventListener("click", onClick));
 
-
-
-arrImages.map((image)=>image.addEventListener ('click',onClick))
-
-modal.addEventListener('click',closeModal)
+modal.addEventListener("click", closeModal);
 
 function onClick(e) {
-    const src =e.target.src;
-   
-    openmodal(src)
+  const src = e.target.src;
+
+  openmodal(src);
 }
 
-    function openmodal(src){
-        modal.style.display='flex'
-        const modalImage = document.createElement('img')
-        body.className = 'overflow-hidden';
-        modalImage.src=src
-        modalImage.className = 'modal-image'
-        modal.append(modalImage)
-        
-    }
+function openmodal(src) {
+  modal.style.display = "flex";
+  const modalImage = document.createElement("img");
+  body.className = "overflow-hidden";
+  modalImage.src = src;
+  modalImage.className = "modal-image";
+  modal.append(modalImage);
+}
 
 function closeModal() {
-   const modalImage=document.querySelector('.modal-image')
-   console.log(modalImage)
-    modal.style.display='none'
-    body.className= 'none'
-    modal.removeChild(modalImage)
-    
+  const modalImage = document.querySelector(".modal-image");
+  console.log(modalImage);
+  modal.style.display = "none";
+  body.className = "none";
+  modal.removeChild(modalImage);
 }
--- main.js
+++ main.js
@@ -1,45 +1,48 @@
-const body = document.getElementById('body')
+const body = document.getElementById("body");
 
-const modal=document.querySelector('.modal')
+const modal = document.querySelector(".modal");
 
-const images = document.querySelectorAll('.vCenter img')
+const images = document.querySelectorAll(".vCenter img");
 
+const arrImages = Array.from(images);
 
-const arrImages = Array.from(images)
+arrImages.map((image) => image.addEventListener("click", onClick));
 
-
-
-arrImages.map((image)=>image.addEventListener ('click',onClick))
-
-modal.addEventListener('click',closeModal)
+modal.addEventListener("click", closeModal);
 
 function onClick(e) {
-    const src =e.target.src;
-    const srcArray=src.split('/')
-    console.log(srcArray)
-    const newsrcArray= [srcArray[0],srcArray[1],srcArray[2],srcArray[3],srcArray[4],'HQ',srcArray[5]]
-    /* const newsrcArray= [srcArray[0],srcArray[1],srcArray[2],srcArray[3],'HQ',srcArray[4]] */
-    
-    const newsrc= newsrcArray.join('/')
-    console.log(newsrc)
-    openmodal(newsrc)
+  const src = e.target.src;
+  const srcArray = src.split("/");
+  console.log(srcArray);
+  const newsrcArray = [
+    srcArray[0],
+    srcArray[1],
+    srcArray[2],
+    srcArray[3],
+    srcArray[4],
+    "HQ",
+    srcArray[5],
+  ];
+  /* const newsrcArray= [srcArray[0],srcArray[1],srcArray[2],srcArray[3],'HQ',srcArray[4]] */
+
+  const newsrc = newsrcArray.join("/");
+  console.log(newsrc);
+  openmodal(newsrc);
 }
 
-    function openmodal(src){
-        const modalImage = document.createElement('img')
-        modalImage.src=src
-        body.className = 'overflow-hidden';
-        modal.style.display='flex'
-        modalImage.className = 'modal-image'
-        modal.append(modalImage)
-        
-    }
+function openmodal(src) {
+  const modalImage = document.createElement("img");
+  modalImage.src = src;
+  body.className = "overflow-hidden";
+  modal.style.display = "flex";
+  modalImage.className = "modal-image";
+  modal.append(modalImage);
+}
 
 function closeModal() {
-   const modalImage=document.querySelector('.modal-image')
-   console.log(modalImage)
-    modal.style.display='none'
-    body.className= 'none'
-    modal.removeChild(modalImage)
-    
+  const modalImage = document.querySelector(".modal-image");
+  console.log(modalImage);
+  modal.style.display = "none";
+  body.className = "none";
+  modal.removeChild(modalImage);
 }
